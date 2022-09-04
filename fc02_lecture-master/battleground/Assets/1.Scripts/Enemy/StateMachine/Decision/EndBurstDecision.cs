f1bc4d9abca962b0f3bdc743024ccfdd827a4c2a
kangjunyoung37 <rkdwnsdud37@gmail.com>
2022-09-05 01:46:36 +0900

    #Decision 생성
    Look,Hear,TakeCover,AdvanceCover,ClearShot,EndBurst,Engage,FeelAlert,ReachedPoint,TargetDead,Waited
    Decision 생성

---
+++ fc02_lecture-master/battleground/Assets/1.Scripts/Enemy/StateMachine/Decision/EndBurstDecision.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 한번 사격이 시작되면 한번에 쏠 수 있는 재장전 전까지 쏠 수 있는 총알의 수를 판단
+/// </summary>
+
+[CreateAssetMenu(menuName ="PluggableAI/Decisions/EndBurst")]
+public class EndBurstDecision : Decision
+{
+    public override bool Decide(StateController controller)
+    {
+        //사격을 많이 했으면 재장전(Wait)
+        return controller.variables.currentShots >= controller.variables.shotsInRounds;
+    }
+}
